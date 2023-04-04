28c1a05d686e27fa63883aa5e6f37ff791aed85f
bennyyy51 <126090605+bennyyy51@users.noreply.github.com>
2023-04-04 18:04:16 -0400

    adding methods
    added nikki's method (getBalance) to the AVLTree class and added my own methods (rotateR, rotateL, add, and print)

---
+++ AVLTree.java
@@ -0,0 +1,149 @@
+
+public class AVLTree {
+
+    Node root = null;
+
+    class Node {
+
+        int data;
+        Node parent;
+        Node left;
+        Node right;
+        int height;
+        int balance;
+
+        Node(int d, Node p) {
+            data = d;
+            parent = p;
+            height = p.height + 1;
+        }
+
+        Node(int d) {
+            data = d;
+            height = 1;
+        }
+
+    }
+
+    AVLTree(Node n) {
+
+        root = n;
+
+    }
+
+    public int getHeight(Node node) {
+        if (node == null) {
+            return 0;
+        } else {
+            int leftHeight = getHeight(node.left);
+            int rightHeight = getHeight(node.right);
+            return 1 + Math.max(leftHeight, rightHeight);
+        }
+
+    }
+
+    public int getBalance(Node n) {
+        int temp;
+        if (n != null) {
+            temp = 1 + getBalance(n.right) - getBalance(n.left);
+            n.balance = temp;
+            return temp;
+        }
+        return 0;
+    }
+
+    public void print(Node root) {
+
+        System.out.println(root);
+        if (root.left != null) {
+            print(root.left);
+        }
+        if (root.right != null) {
+            print(root.right);
+        }
+    }
+
+    int balance(Node node) {
+        if (node == null)
+            return 0;
+
+        return getHeight(node.left) - getHeight(node.right);
+    }
+
+    public Node add(Node node, int data) {
+        if (node == null) {
+            return new Node(data);
+        }
+
+        if (data < node.data) {
+            node.left = add(node.left, data);
+        }
+
+        else if (data > node.data) {
+            node.right = add(node.right, data);
+        }
+
+        else {
+            return node;
+        }
+
+        node.height = 1 + getHeight(node);
+
+        int balance = getBalance(node);
+
+        if (balance > 1 && data < node.left.data) {
+            return rotateR(node);
+        }
+        
+        if (balance < -1 && data > node.right.data) {
+            return rotateL(node);
+        }
+        
+        if (balance > 1 && data > node.left.data) {
+            node.left = rotateL(node.left);
+            return rotateR(node);
+        }
+        
+        if (balance < -1 && data < node.right.data) {
+            node.right = rotateR(node.right);
+            return rotateL(node);
+        }
+        
+        return node;
+        
+    }
+
+    public Node rotateR(Node node) {
+        Node nodeL = node.left;
+        Node nodeLR = nodeL.right;
+        
+        nodeL.right = node;
+        node.left = nodeLR;
+        
+        nodeL.height = getHeight(nodeL);
+        node.height = getHeight(node);
+        
+        return nodeL;
+
+    }
+
+    public Node rotateL(Node node) {
+        
+        Node nodeR = node.right;
+        Node nodeRL = nodeR.left;
+        
+        nodeR.left = node;
+        node.right = nodeRL;
+        
+        node.height = getHeight(node);
+        nodeR.height = getHeight(nodeR);
+        
+        return nodeR;
+
+    }
+    
+    public static void main(String[] args) {
+        
+
+    }
+}
